f9e5630ae8273c274e0df977f196f6899c4951bf
sayingu <sayingu@gmail.com>
2017-02-02 01:37:45 +0900

    Update database_init.py

--- database_init.py
+++ database_init.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 from database_setup import Base, User, Category, CategoryItem
 
-engine = create_engine('sqlite:///var/www/udacity-catalog/catalog.db')
+engine = create_engine('postgresql://catalog:catalog@localhost/catalog')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
